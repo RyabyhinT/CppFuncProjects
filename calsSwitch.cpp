f71e227e7afa9edf8523410684297ee061b60dff
RyabyhinT <144794994+RyabyhinT@users.noreply.github.com>
2023-09-26 13:04:55 +0300

    create calc(switch/case)
    Калькулятор switch/case

---
+++ calsSwitch.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    int b, c;
+
+    char a;
+    cout << "Введите действие:\n ";
+    cin >> a;
+    cout << "Введите числа\n";
+    cin >> b;
+    cin >> c;
+
+
+    switch (a) {
+    case '+':
+        cout << b + c;
+        break;
+    case '-':
+        cout << b - c;
+        break;
+    case '*':
+        cout << b * c;
+        break;
+    case '/':
+        if (c == 0) cout << "На ноль делить нельзя";
+
+        else cout << b / c;
+
+        break;
+    default:
+        cout << "Введено неверное действие ";
+    }
+}
